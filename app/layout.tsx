ae65a30c7d72b013e088adf5c6abacf0be7ae017
Aydin Joshi <ajoshi1336@gmail.com>
2024-06-19 00:14:29 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lawn Mowing - Mow & Snow: Year Round Property Maintenance",
+  description: "Year Round Property Maintenance in North York Region",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
